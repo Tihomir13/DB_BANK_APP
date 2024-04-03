c8a6e055708008ea1d36cef147d50382754b0642
Tihomir13 <tihomir13n2002@abv.bg>
2024-04-03 16:02:42 +0300

    fixing js

--- Credits.php
+++ Credits.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>User Dashboard</title>
     <link rel="stylesheet" href="Style\styles.css" />
-    <script defer src="./helperJS.js"></script>
+    <script src="./helperJS.js"></script>
   </head>
   <body>
     <div class="container">
